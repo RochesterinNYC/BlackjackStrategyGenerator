95e0454f75d7ee92ae0edac7d6dbb1e11e883cc8
James Wen <jrw2175@columbia.edu>
2013-10-21 17:55:27 -0400

    Implemented handSum subfunction

--- blackjackStrategy.m
+++ blackjackStrategy.m
@@ -54,3 +54,18 @@ function createPlayerHand[playerHand] = createPlayerHand(handType)
     playerHand[1, end + 1] = deal(1, handType - 25);
   end
 end
+%------------------------------------------------------------------
+%handSum Function
+%Calculates sum of hand (necessary to have function because of special nature of aces)
+function[handValue] = handSum(hand) 
+  if(min(dealerHand) == 1)
+    %number of aces in hand does not matter since only one ace can have a value of 11 instead of 1 (two aces with value of 11 is 22 hand total)
+    if sum(hand) + 10 <= 21
+      handValue = sum(hand) + 10;
+    else
+      handValue = sum(hand); 
+    end
+  else
+    handValue = sum(hand);
+  end
+end
