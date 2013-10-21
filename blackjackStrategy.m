1fa52989feab50e90df4949aa89c03ae9008b4d8
James Wen <jrw2175@columbia.edu>
2013-10-21 17:56:42 -0400

    Implemented primary functionality and playRound subfunction

--- blackjackStrategy.m
+++ blackjackStrategy.m
@@ -1,5 +1,46 @@
 function[houseEdge] = blackjackStrategy(numDecks, standSoft17, doubleAfterSplit, blackjackPayout, numSimulations)
+  %create cell array of every combination (35 rows x 10 columns)
+  stratTable = zeros(35, 10)
+  [playerHands, dealerCards] = size(stratTable);
 
+  %Create Arrays:
+  %playerOptions 1-4 = Respectively Stand, Hit, Double, Split;
+  playerOptions = [1, 2, 3, 4]
+  playerOptionsWins = zeroes(1, 4);
+
+  %create deck
+  %matrix of 10 * 4 (10, J, Q, K are all the same value)
+  global decks = zeros(10, 4);
+  %creates a big deck of cards that are composed of numDecks decks
+  setDeck(numDecks);
+
+  totalWins = 0;
+
+  %Iterate through each combination
+  %"Wins" are in terms of money won so winning on a double nets you twice as much money as a regular win would and a blackjack win would net you money equal to blackjack payout rate * what you bet
+  for playerHandType = 1:playerHands
+    for dealerCard = 1:dealerCards
+      playerOptionWins(:, :) = 0;
+      for playerOption = 1:playerOptions
+        for n = 1:numSimulations
+          if playOption == 3 && playRound(playerHandType, dealerCard, playerOption) == 1
+            playerOptionWins(playerOption) = playerOptionWins(playerOption) + 2;
+          if playRound(playerHandType, dealerCard, playerOption) == 1
+            playerOptionWins(playerOption) = playerOptionWins(playerOption) + 1;
+          else if playRound(playerHandType, dealerCard, playerOption) == 2
+            playerOptionWins(playerOption) = playerOptionWins(playerOption) + blackjackPayout;
+          end
+        end
+      end
+      %compareWinFrequencies for player options for the currently matched up scenario
+      %and set the appropriate cell matrix equal to the option to choose
+      totalWins = totalWins + max(playerOptionWins);
+      stratTable(playerHandType, dealerCard)find(playerOptionWins == max(playerOptionWins));
+    end
+  end
+
+  %generate chart
+  houseEdge = totalWins / (numSimulations * 350);
 end
 %------------------------------------------------------------------
 %setDeck Function
@@ -68,4 +109,53 @@ function[handValue] = handSum(hand)
   else
     handValue = sum(hand);
   end
+end
+%------------------------------------------------------------------
+%playRound Function
+%Plays a simulated round of blackjack where user has a predefined hand, action to take, and dealer has a faceup card
+%Dealer plays as normal 
+%playerWon as 0 is loses, 1 is wins, 2 is player blackjack
+function[playerWon] = playRound(userHandType, dealerCard, playerAction, standSoft17, doubleAfterSplit)
+
+  playerWon = -1;
+  setDeck(numDecks);
+  dealerHand = [dealCard(0, dealerCard), dealCard()];
+  playerHand = createPlayerHand(userHandType);
+
+  %Both Blackjack (Tie)
+  if handSum(userHand) == 21 && handSum(dealerHand) == 21
+    playerWon = playRound(userHand, dealerCard, playerAction, standSoft17, doubleAfterSplit);
+    return
+  end
+
+  %Player Blackjack (Player Win)
+  if handSum(userHand) == 21 && handSum(dealerHand) ~= 21
+    playerWon = 2;
+    return
+  end
+
+  %Dealer Blackjack (Player Loss)
+  if handSum(userHand) ~= 21 && handSum(dealerHand) == 21
+    playerWon = 0;
+    return
+  end
+
+  %Else play out
+  %playerAction == 1, then stand
+  if playerAction == 2 || playerAction == 3 %Hit or Double
+    playerHand[1, end + 1] = deal();
+  end
+  if playerAction == 4 %Split
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FIXXXXX
+    playerHand[1, end + 1] = deal();
+  end
+
+  %tie (push) has no effect on strategic selection or wins vs. loses so play again recursively
+  if handSum(userHand) == handSum(dealerHand)
+    playerWon = playRound(userHand, dealerCard, playerAction, standSoft17, doubleAfterSplit);
+    return
+  end
+
+  playerWon = ~(handSum(userHand) > handSum(dealerHand));
+
 end
